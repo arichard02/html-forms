f460272af5d9b65bb54575ccacb0bc46183f79b9
A <arichard02@aol.com>
2022-07-12 01:40:53 -0400

    added picture and styling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
-  <h1>Vehicle Inqire Form</h1>
+  <h1>Vehicle Inquiry Form</h1>
+  <img src="https://d18zm77o7qzu1y.cloudfront.net/uploads/files/000/005/469/original/toyota_.jpg?1475163285" alt="Image of Cars">
   <a href="https://vitejs.dev/guide/features.html" target="_blank"</a>
 `
